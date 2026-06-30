c9f166eb7e544eee7a85db7a732a4497f6316ca8
Matthew Zhong <matthewzhong@logmethods.com>
2026-06-30 02:38:06 +0000

    add ping to wikipedia because it's hard to source finance data

--- go/main.go
+++ go/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"net/http"
 	"os"
 	"time"
 
@@ -10,11 +11,29 @@ import (
 )
 
 func main() {
-	if len(os.Args) != 3 || os.Args[1] != "db" || os.Args[2] != "ping" {
-		fmt.Fprintln(os.Stderr, "usage: sp500ctl db ping")
+	if len(os.Args) != 3 {
+		usage()
 		os.Exit(2)
 	}
 
+	switch {
+	case os.Args[1] == "db" && os.Args[2] == "ping":
+		dbPing()
+	case os.Args[1] == "wiki" && os.Args[2] == "ping":
+		wikiPing()
+	default:
+		usage()
+		os.Exit(2)
+	}
+}
+
+func usage() {
+	fmt.Fprintln(os.Stderr, "usage:")
+	fmt.Fprintln(os.Stderr, "  sp500ctl db ping")
+	fmt.Fprintln(os.Stderr, "  sp500ctl wiki ping")
+}
+
+func dbPing() {
 	dsn := os.Getenv("DATABASE_URL")
 	if dsn == "" {
 		dsn = "postgres://postgres:postgres@postgres:5432/postgres?sslmode=disable"
@@ -36,4 +55,29 @@ func main() {
 	}
 
 	fmt.Println("connected to postgres")
+}
+
+func wikiPing() {
+	url := "https://en.wikipedia.org/w/api.php?action=parse&page=List_of_S%26P_500_companies&prop=wikitext&format=json"
+
+	client := &http.Client{
+		Timeout: 10 * time.Second,
+	}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "request creation failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	req.Header.Set("User-Agent", "sp500-tracker/0.1")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "request failed: %v\n", err)
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+
+	fmt.Printf("Wikipedia API status: %s\n", resp.Status)
 }
